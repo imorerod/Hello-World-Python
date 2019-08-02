a50e745d2eb3f9b65bcaf723a7f758210eede991
Isaac <imorerod@hotmail.com>
2019-08-02 11:30:42 -0500

    changed character_name variable

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 
-character_name = 'John'
+character_name = 'Tom'
 character_age = '35'
 print('There once was a man named ' + character_name + ', ')
 print('he was ' + character_age + ' years old. ')
