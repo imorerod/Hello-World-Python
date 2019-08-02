330b480f4a61c87f55368734459fba99bc7dcd93
Isaac <imorerod@hotmail.com>
2019-08-02 11:29:05 -0500

    created application utilizing two variables and concatenation

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-print(' .  /|')
-print('  /  |')
-print(' /   |')
-print('/____|')
+
+character_name = 'John'
+character_age = '35'
+print('There once was a man named ' + character_name + ', ')
+print('he was ' + character_age + ' years old. ')
+print('He really liked the name ' + character_name + ', ')
+print('but did not like being ' + character_age + '.')
