e9bd86ef666e23e3640f3bdd5b5d74be3d554f32
Isaac <imorerod@hotmail.com>
2019-08-05 09:13:36 -0500

    found floor, ceil, and sqrt of numbers

--- numbers.py
+++ numbers.py
@@ -1,3 +1,6 @@
+# pulls in many math functions
+from math import *
+
 print(2)
 print(-2)
 print(3 * 56)
@@ -11,3 +14,29 @@ print(my_num)
 # makes my_num a string
 # must use 'str' w/ numbers if concatenating with a string
 print(str(my_num))
+
+# absolute value
+my_num = -5
+print(abs(my_num))
+
+# give this function a number and power
+# 3 raised to the power of 2
+print(pow(3, 2))
+
+# tells which num is the largest
+print(max(4,6))
+
+# tells which num is the smallest
+print(min(4,6))
+
+# round the num
+print(round(4.453))
+
+# drops off the decimal place
+print(floor(3.7))
+
+# automatically rounds up to next whole number
+print(ceil(4.358))
+
+# finds square root
+print(sqrt(56))
