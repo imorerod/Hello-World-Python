1fc2474f6b95aa2c0e90b6750dce019aae228227
Isaac <imorerod@hotmail.com>
2019-08-05 09:06:03 -0500

    converted number into string

--- numbers.py
+++ numbers.py
@@ -3,3 +3,11 @@ print(-2)
 print(3 * 56)
 # handles PEMDAS correctly
 print(3 + 5 * 8)
+# '%' is modulus operator; takes 10 / 3, then prints the remainder
+print(10 % 3)
+
+my_num = 5
+print(my_num)
+# makes my_num a string
+# must use 'str' w/ numbers if concatenating with a string
+print(str(my_num))
