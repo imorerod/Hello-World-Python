c84cf084d12313b29c901fe64a3c597d29a055ab
Isaac <imorerod@hotmail.com>
2019-08-08 09:44:35 -0500

    finding value using negative indices

---
+++ lists.py
@@ -0,0 +1,9 @@
+
+# can put strings, numbers or booleans
+friends = ['Morgan', 'Miguel', 'Tyler', 'Ryan', 'Jacob']
+
+# index 2 of friends list/array
+print(friends[2])
+
+# starts indexing at -1 (NOT 0) from the end of the list
+print(friends[-2])
