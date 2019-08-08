3e48728cb6446174aee4ee4d54c2787e77a133b0
Isaac <imorerod@hotmail.com>
2019-08-08 12:59:13 -0500

    created say_hi function

---
+++ functions.py
@@ -0,0 +1,14 @@
+
+# 'def' is a key word to use a function
+# Python naming convention is to use lowercase, snake-case for:
+# 1) variables 2) methods 3) functions
+
+
+def say_hi():
+    print('Hello User')
+
+
+say_hi()
+
+
+
