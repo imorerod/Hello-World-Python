669b5cb7bee8d48d5dd07ff0d8daebbb18f49bb9
Isaac <imorerod@hotmail.com>
2019-08-08 09:49:34 -0500

    modified value at a specific index

--- lists.py
+++ lists.py
@@ -7,3 +7,13 @@ print(friends[2])
 
 # starts indexing at -1 (NOT 0) from the end of the list
 print(friends[-2])
+
+# prints value at index 1 and greater
+print(friends[1:])
+
+# prints value at index 1 & 2 (DOES NOT INCLUDE 3)
+print(friends[1:3])
+
+# modify value at index 4
+friends[4] = 'Bob'
+
