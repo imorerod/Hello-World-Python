9c0f1dece614788b09150827c4dda6e0d2cb495d
imorerod <idm9191@gmail.com>
2019-08-09 12:06:10 -0500

    added "else if statements"

--- if-statements.py
+++ if-statements.py
@@ -1,10 +1,23 @@
 
-is_male = False
+is_male = True
+is_tall = False
 
-if is_male:
-    print('You are a male.')
+# 'or' keyword
+if is_male or is_tall:
+    print('You are a male or tall or both.')
 else:
-    print('You are not a male.')
+    print('You are neither a male nor tall.')
+
+# 'and' keyword
+if is_male and is_tall:
+    print('You are a tall male.')
+elif is_male and not is_tall:
+    print('You are a short male.')
+elif not is_male and is_tall:
+    print('You are not male but tall.')
+else:
+    print('You are neither a male or tall or both.')
+
 
 
 
