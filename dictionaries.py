db9afd7a906cda7e36bcc07f72ffcd7e472ad642
imorerod <idm9191@gmail.com>
2019-08-12 13:12:55 -0500

    created all key value pairs

--- dictionaries.py
+++ dictionaries.py
@@ -1,5 +1,16 @@
 
 monthConversions = {
-
+    'Jan': 'January',
+    'Feb': 'February',
+    'March': 'March',
+    'Apr': 'April',
+    'May': 'May',
+    'Jun': 'June',
+    'Jul': 'July',
+    'Aug': 'August',
+    'Sept': 'September',
+    'Oct': 'October',
+    'Nov': 'November',
+    'Dec': 'December',
 }
 
