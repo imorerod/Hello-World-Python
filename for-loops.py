be1a3dee593f7fe48b9b2f5aba666d73fcb458b6
imorerod <isaac.morerod@gmail.com>
2019-08-29 14:07:50 -0500

    created for loop for range of numbers

---
+++ for-loops.py
@@ -0,0 +1,12 @@
+
+for letter in 'Prime Digital Academy':
+    print(letter)
+
+friends = ['Jim', 'Pam', 'Kevin']
+for friend in friends:
+    print(friend)
+
+# 0-9; 10 not included
+for index in range(10):
+    print(index)
+
