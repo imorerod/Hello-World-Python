e19feaa6de8bb6917b62e7ac46a62af86dfb0294
imorerod <isaac.morerod@gmail.com>
2019-08-29 14:14:53 -0500

    used if/else inside range for-loop

--- for-loops.py
+++ for-loops.py
@@ -15,6 +15,15 @@ for index in range(3, 10):
     print(index)
 
 # creates the range; which is whatever the length of the array is
+# len(friends) = 3
 for index in range(len(friends)):
     print(friends[index])
+
+for index in range(5):
+    if index == 0:
+        print('first iteration')
+    else:
+        print('not first iteration')
+
+
 
