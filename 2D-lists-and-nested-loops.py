9551346b09c332e26754fa45a67f9e5edb996817
imorerod <isaac.morerod@gmail.com>
2019-09-03 16:56:57 -0500

    access specific value within a grid

--- 2D-lists-and-nested-loops.py
+++ 2D-lists-and-nested-loops.py
@@ -1,5 +1,6 @@
 
 # viewed as a grid; 4 rows, 3 columns
+# rows and columns both start at '0'; not 1
 number_grid = [
     [1, 2, 3],
     [4, 5, 6],
@@ -7,5 +8,7 @@ number_grid = [
     [0]
 ]
 
+# [row][column]
+print(number_grid[2][2])
 
 
